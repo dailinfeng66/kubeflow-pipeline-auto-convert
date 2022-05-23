b9addd71920a8bb5e48e6b8c33bec95990d3825c
戴林峰 <dailinfeng66@163.com>
2022-05-23 14:48:29 +0800

    垃圾蒋文

--- ast_convert/resource/递归引入方法/A.py
+++ ast_convert/resource/递归引入方法/A.py
@@ -1,8 +1,16 @@
+from resource.递归引入方法.B import func_2
 
 
 def func_3():
     res = func_2(1)
     return res
+
+
+def func_c_wrap():
+    res3 = func_3()
+    return res3
+
+
 
 def func_4():
     res = func_2(1)
